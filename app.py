f13a9f0c7610b55ac16e252b7bf93840933d44da
25010711-source <25010711@pusangangseo.hs.kr>
2025-12-18 13:59:32 +0900

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("간단한 사용자 입력 앱")
-
-name = st.text_input("이름을 입력하세요:")
-age = st.number_input("나이를 입력하세요:", min_value=0, max_value=120)
-
-if st.button("확인"):
-    st.success(f"안녕하세요 {name}님! 당신의 나이는 {age}살 입니다.")
